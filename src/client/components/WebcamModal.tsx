362d48c7a1d20917546d2e2e9fce5ead48746146
Fokam Manuel <arnolfokam23@gmail.com>
2021-11-30 00:58:39 +0200

    fix model scrolling

--- src/client/components/WebcamModal.tsx
+++ src/client/components/WebcamModal.tsx
@@ -18,7 +18,7 @@ const WebcamModal = ({ setHandleCloseModal, setvalidatePicture }: WebcamModalPro
   );
 
   return (
-    <div id="bio-modal" className="fixed z-10 inset-0 overflow-y-auto" aria-labelledby="modal-title" role="dialog"
+    <div id="bio-modal" className="absolute z-10 inset-0 overflow-y-auto" aria-labelledby="modal-title" role="dialog"
       aria-modal="true">
       <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
         <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" aria-hidden="true"></div>
